cb424a5ba0cd27736b946cd30575ab7748f629e4
jsdiy <rkamiya2@gmail.com>
2026-02-16 23:46:55 +0900

    Add files via upload

--- lib/GLcdLib/GLcdText/GLcdText.cpp
+++ lib/GLcdLib/GLcdText/GLcdText.cpp
@@ -13,15 +13,15 @@ void	GLcdText::Initialize()
 }
 
 //文字の色（前景色）を設定する
-void	GLcdText::SetTextColor(const Color* color)
+void	GLcdText::SetTextColor(const Color& color)
 {
-	foreColor = *color;
+	foreColor = color;
 }
 
 //文字画面の色（背景色）を設定する
-void	GLcdText::SetTextBgColor(const Color* color)
+void	GLcdText::SetTextBgColor(const Color& color)
 {
-	bgColor = *color;
+	bgColor = color;
 }
 
 //描画の拡大率を設定する
@@ -52,13 +52,13 @@ void	GLcdText::GetCharSize(int16_t* charWidth, int16_t* charHeight)
 }
 
 //桁位置（0以上）のx座標
-int16_t	GLcdText::PointX(uint8_t column) const
+int16_t	GLcdText::XFromCol(uint8_t column) const
 {
 	return scaledCharW * column;
 }
 
 //行位置（0以上）のy座標
-int16_t	GLcdText::PointY(uint8_t row) const
+int16_t	GLcdText::YFromRow(uint8_t row) const
 {
 	return scaledCharH * row;
 }
@@ -84,12 +84,12 @@ int16_t	GLcdText::DrawChar(int16_t x, int16_t y, char cc)
 }
 
 //色データを画素数分、画像バッファへ出力する
-size_t	GLcdText::WriteColorToImageBuffer(const Color* color, int16_t repeat, size_t bufIndex, uint8_t* imgBuf)
+size_t	GLcdText::WriteColorToImageBuffer(const Color& color, int16_t repeat, size_t bufIndex, uint8_t* imgBuf)
 {
 	while (repeat--)
 	{
-		imgBuf[bufIndex++] = color->Bytes[0];
-		imgBuf[bufIndex++] = color->Bytes[1];
+		imgBuf[bufIndex++] = color.Bytes[0];
+		imgBuf[bufIndex++] = color.Bytes[1];
 	}
 	return bufIndex;
 }
@@ -112,7 +112,7 @@ void	GLcdText::DrawCharToImageBuffer(const uint8_t* fontDatas, uint8_t* imgBuf)
 			Color& color = (pixel != 0) ? foreColor : bgColor;
 
 			//拡大倍数の分、1画素の色データを出力する
-			bufIdx = WriteColorToImageBuffer(&color, widthScale, bufIdx, imgBuf);
+			bufIdx = WriteColorToImageBuffer(color, widthScale, bufIdx, imgBuf);
 		}
 
 		//いま出力した1行を拡大倍数の分、画像バッファ上で2行目、3行目…へとコピーする
@@ -212,5 +212,41 @@ int16_t	GLcdText::DrawUInt(int16_t x, int16_t y, uint32_t n)
 	while (0 < n);
 	
 	while (i != 0) { x = DrawChar(x, y, '0' + kbuf[--i]); }
+	return x;
+}
+
+//ラベル付きDrawWord
+int16_t	GLcdText::DrawWord(int16_t x, int16_t y, const char* labelPre, uint16_t n, const char* labelPost = nullptr)
+{
+	x = DrawString(x, y, labelPre);
+	x = DrawWord(x, y, n);
+	if (labelPost != nullptr) { x = DrawString(x, y, labelPost); }
+	return x;
+}
+
+//ラベル付きDrawByte
+int16_t	GLcdText::DrawByte(int16_t x, int16_t y, const char* labelPre, uint8_t n, const char* labelPost = nullptr)
+{
+	x = DrawString(x, y, labelPre);
+	x = DrawByte(x, y, n);
+	if (labelPost != nullptr) { x = DrawString(x, y, labelPost); }
+	return x;
+}
+
+//ラベル付きDrawInt
+int16_t	GLcdText::DrawInt(int16_t x, int16_t y, const char* labelPre, int32_t n, const char* labelPost)
+{
+	x = DrawString(x, y, labelPre);
+	x = DrawInt(x, y, n);
+	if (labelPost != nullptr) { x = DrawString(x, y, labelPost); }
+	return x;
+}
+
+//ラベル付きDrawUInt
+int16_t	GLcdText::DrawUInt(int16_t x, int16_t y, const char* labelPre, uint32_t n, const char* labelPost)
+{
+	x = DrawString(x, y, labelPre);
+	x = DrawUInt(x, y, n);
+	if (labelPost != nullptr) { x = DrawString(x, y, labelPost); }
 	return x;
 }
